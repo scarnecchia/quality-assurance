a90bd61e492e73586b98f9cc7ae396e70a5af5b8
D Scarnecchia <daniel_scarnecchia@hphci.harvard.edu>
2026-03-09 20:18:15 -0700

    chore: export global check functions from validation package

--- src/scdm_qa/validation/__init__.py
+++ src/scdm_qa/validation/__init__.py
@@ -1,4 +1,11 @@
+from scdm_qa.validation.global_checks import check_sort_order, check_uniqueness
 from scdm_qa.validation.results import StepResult, ValidationResult
 from scdm_qa.validation.runner import run_validation
 
-__all__ = ["StepResult", "ValidationResult", "run_validation"]
+__all__ = [
+    "StepResult",
+    "ValidationResult",
+    "run_validation",
+    "check_sort_order",
+    "check_uniqueness",
+]
