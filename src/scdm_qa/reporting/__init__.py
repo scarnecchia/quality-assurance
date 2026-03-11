2957a612e61bc31be798e69ca7ec9697639c059e
D Scarnecchia <daniel_scarnecchia@hphci.harvard.edu>
2026-03-11 11:22:52 -0700

    refactor: update reporting package exports for dashboard

--- src/scdm_qa/reporting/__init__.py
+++ src/scdm_qa/reporting/__init__.py
@@ -1,6 +1,4 @@
-from scdm_qa.reporting.builder import save_table_report
 from scdm_qa.reporting.dashboard import save_dashboard
-from scdm_qa.reporting.index import make_report_summary, save_index
 from scdm_qa.reporting.serialise import serialise_run
 
-__all__ = ["save_table_report", "save_index", "make_report_summary", "serialise_run", "save_dashboard"]
+__all__ = ["save_dashboard", "serialise_run"]
